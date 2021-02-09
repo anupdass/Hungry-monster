6d04726477ed038c0b0680abcd319f5512505e9f
anupdass <anupdas633@gmail.com>
2021-02-09 09:16:23 +0600

    lastCOmmit

--- app.js
+++ app.js
@@ -19,15 +19,35 @@ const createMeals = (meals) => {
         const mealInfo = `
         <img src="${meal.strMealThumb}"> 
         <h3>${meal.strMeal}</h3>
+        <button onclick="singleCountryDetails(${meal.idMeal})">ClickMe</button> 
         `;
         mealDiv.innerHTML = mealInfo;
         mealsElement.appendChild(mealDiv);
 
-        const id = meal.idMeal;
-        // single Meal Details .......
-        mealDiv.addEventListener('click', function(){
-            alert("I can't do item details. Please make a Video Solve this problem. and Upload in a Module");
-        })
+
+        // mealDiv.addEventListener('click', function(){
+        //     alert("I can't do item details. Please make a Video Solve this problem. and Upload in a Module");
+        // })
     }
+
+}
+
+const singleCountryDetails = id => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => renderingMeal(data.meals));
+}
+
+const renderingMeal = meal => {
+    const mealDetails = document.getElementById('meals-details');
+    mealDetails.className = 'ingredient';
+
+    mealDetails.innerHTML = `
+    <img src = ${meal[0].strMealThumb}>
+    <h2>${meal[0].strMeal}</h2>
+    `
+    mealDetails.appendChild(singleMealDiv);
 
 }
